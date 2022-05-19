49e9cc25a14d2d1e7c44294513c227d413035b6d
Karthikeyan <sent2tk@gmail.com>
2022-05-19 14:02:09 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	challenge01.py

--- challenge01.py
+++ challenge01.py
@@ -1,7 +1,1 @@
-<<<<<<<<< Temporary merge branch 1
-
-=========
-import pandas as pd
-a=pd.DataFrame(pd.read_excel("https://github.com/Karthikeyan-thangavel/challenge-01/blob/2df621bba8dd44ce5c0ad3a708f11c9c7e935144/table_data.xlsx"))
-print(a)
->>>>>>>>> Temporary merge branch 2
+print('hai')
