abfe3a22c15e819f0c58c1a544e9a1d710a58a8d
Karthikeyan <sent2tk@gmail.com>
2022-05-19 15:13:30 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	challenge01.py

--- challenge01.py
+++ challenge01.py
@@ -1,5 +1,5 @@
 import pandas as pd
-a=pd.DataFrame(pd.read_excel("C:\\Users\\Karthikeyan\\Documents\\GitHub\\challenge-01\\table_data.xlsx",sheet_name=1))
+a=pd.DataFrame(pd.read_excel(,sheet_name=1))
 b=pd.DataFrame(pd.read_excel("C:\\Users\\Karthikeyan\\Documents\\GitHub\\challenge-01\\table_data.xlsx",sheet_name=2))
 c=a.loc[a["id"].isin(b["employee_id"])==False]
 print(c["first_name"]+" "+c["last_name"])
