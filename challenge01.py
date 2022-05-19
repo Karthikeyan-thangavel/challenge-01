7b17c4e99afba35e3d76bb32845f349d06166fdf
Karthikeyan <81585916+Karthikeyan-thangavel@users.noreply.github.com>
2022-05-19 15:13:57 +0530

    Update challenge01.py

--- challenge01.py
+++ challenge01.py
@@ -1,4 +1,6 @@
 import pandas as pd
+
+ur="https://view.officeapps.live.com/op/view.aspx?src=https%3A%2F%2Fraw.githubusercontent.com%2FKarthikeyan-thangavel%2Fchallenge-01%2Fmain%2Ftable_data.xlsx&wdOrigin=BROWSELINK"
 a=pd.DataFrame(pd.read_excel("C:\\Users\\Karthikeyan\\Documents\\GitHub\\challenge-01\\table_data.xlsx",sheet_name=1))
 b=pd.DataFrame(pd.read_excel("C:\\Users\\Karthikeyan\\Documents\\GitHub\\challenge-01\\table_data.xlsx",sheet_name=2))
 c=a.loc[a["id"].isin(b["employee_id"])==False]
